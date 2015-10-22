c2c5a1f49ce9227014fea799b3a0cb1a753d0a58
douglas bean <dbean1515@gmail.com>
2015-10-22 13:16:08 -0500

    Encapsulate Angular code in immediate function, move variables and a method, rename refactor

--- app.js
+++ app.js
@@ -1,168 +1,191 @@
-var app = angular.module("myApp", []); //empty array is needed, even if there are no dependencies
-
-app.controller("dataController",  ['$scope',  '$http', function($scope, $http) {
+(function(angular) {
 	
-	$scope.currentPage = 0; 
-	$scope.itemsPerPage = 5;
-	$scope.users = []; 
-	$scope.pagedList = []; 
-	$scope.filtered = [];
-	$scope.searchText = ''; 
+	'use strict';
+	var app = angular.module("myApp", []); 
+	app.controller("dataController",  ['$scope',  '$http', function($scope, $http) {
+		
+		var users = []; 
+		var reverseSort = false;
+		
+		$scope.currentPage = 0; 
+		$scope.itemsPerPage = 5; //todo: make a text box for this or remove it from $scope
+		$scope.pagedList = []; 
+		$scope.searchText = ''; 
+		
+		$scope.setPage = function(pageIndex){
+			$scope.currentPage = pageIndex;
+		};
 
-	$scope.paginate = function (list) {
-	    $scope.pagedList = [];
-	    for (var i = 0; i < list.length; i++) {
-	        if (i % $scope.itemsPerPage === 0) {
-	            $scope.pagedList[Math.floor(i / $scope.itemsPerPage)] = [list[i]];
-	        } else {
-	            $scope.pagedList[Math.floor(i / $scope.itemsPerPage)].push(list[i]);
-	        }
-	    }
-	    $scope.currentPage = 0;
-	};
-	
-	$scope.paginateOnClick = function(pageIndex){
-	    $scope.currentPage = pageIndex;
-	};
+		$scope.onBlur = function () {
+			setPagination();
+		};
 
-	$scope.onBlur = function () {
-	    setPagination();
-	};
+		$scope.keyPress = function () {
+			setPagination();
+		}; 
+		
+		function setPagination() {
 
-	$scope.keyPress = function () {
-	    setPagination();
-	};
+			var filtered = search($scope.searchText);
 
-	function setPagination() {
+			if (filtered.length > 0) {
+				paginate(filtered);
+			} else {
+				if (users.length > 0 && $scope.itemsPerPage > 0) {
+					paginate(users);
+				}
+			}
+		};
+		
+		function search(criteria){
 
-	    $scope.filtered = search($scope.users, $scope.searchText);
+			var filtered = [];
 
-	    if ($scope.filtered.length > 0) {
-	        $scope.paginate($scope.filtered);
-	    } else {
-	        if ($scope.users.length > 0 && $scope.itemsPerPage > 0) {
-	            $scope.paginate($scope.users);
-	        }
-	    }
-	};
-	
-	function search(users, criteria){
+			if (users && users.length > 0 && (criteria))
+			{	
+			   for (var index in users) {
+			   
+				   var global_match = "g";
+				   var regExp = new RegExp(criteria, global_match);	   
+				   
+				   var first_name = users[index].first_name;	    
+				   var last_name = users[index].last_name;
+				   var country = users[index].country;
+				
+				   var first_name_result = first_name.match(regExp);	   
+				   var last_name_result = last_name.match(regExp);
+				   var country_result = country.match(regExp);
+				
+				   if (first_name_result !== null) {
+					   if (first_name_result == users[index].first_name) { 
+						   filtered.push(users[index]);
+					   }
+				   }
 
-	    var filtered = [];
+				   if (last_name_result !== null) {
+					   if (last_name_result == users[index].last_name) { 
+						   filtered.push(users[index]);
+					   }
+				   }
 
-	    if (users && users.length > 0 && (criteria))
-	    {	
-	       for (var index in users) {
-	       
-	           var regExp = new RegExp(criteria, "g");	   
-	           var first_name = users[index].first_name;	    
-	           var last_name = users[index].last_name;
-	           var country = users[index].country;
-	        
-	           var first_name_result = first_name.match(regExp);	   
-	           var last_name_result = last_name.match(regExp);
-	           var country_result = country.match(regExp);
-            
-	           if (first_name_result !== null) {
-	               if (first_name_result == users[index].first_name) { 
-	                   filtered.push(users[index]);
-	               }
-	           }
+				   if (country_result !== null) {
+					   if (country_result == users[index].country) { 
+						   filtered.push(users[index]);
+					   }
+				   }
+			 }
+		  }
+		  return filtered;
+		}
 
-	           if (last_name_result !== null) {
-	               if (last_name_result == users[index].last_name) { 
-	                   filtered.push(users[index]);
-	               }
-	           }
+		function paginate(list) {
+			setTwoDimensionalArray(list);
+		};
+		
+		function setTwoDimensionalArray(list) {
+			$scope.pagedList = [];
+			for (var i = 0; i < list.length; i++) {
+				
+				var innerArrayIndex = getRemainder(i, $scope.itemsPerPage);
+				var pageIndex = getPageIndex(i, $scope.itemsPerPage);
+				
+				if (innerArrayIndex === 0) {
+					$scope.pagedList[pageIndex] = [list[i]];
+				} else {
+					$scope.pagedList[pageIndex].push(list[i]);
+				}
+			}
+			$scope.currentPage = 0;
+		}
+		
+		function getRemainder(index, itemsPerPage) {
+			return (index % itemsPerPage);
+		}
+		
+		function getPageIndex(index, itemsPerPage) {
+			return Math.floor(index / itemsPerPage);
+		}
+		
+		$scope.sort = function(header){
+		   if(reverseSort === false){
+			  users = sortByKeyAscending(users, header);
+			  reverseSort = true;
+		   }else{
+			  users = sortByKeyDescending(users, header);
+			  reverseSort = false;
+			}
+			paginate(users);
+		};  
+		  
+		function sortByKeyAscending(array, key) {
+			return array.sort(function(a, b) {
+				var x = a[key]; var y = b[key];
+				return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+			});
+		 }
+		   
+		 function sortByKeyDescending(array, key) {
+			return array.sort(function(a, b) {
+				var x = a[key]; var y = b[key];
+				return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+			});
+		 }
 
-	           if (country_result !== null) {
-	               if (country_result == users[index].country) { 
-	                   filtered.push(users[index]);
-	               }
-	           }
-	     }
-	  }
-	  return filtered;
-    }
-   
-	$scope.reverseSort = false;
-	$scope.sort = function(header){
-	   if($scope.reverseSort === false){
-	      $scope.users = sortByKeyAscending($scope.users, header);
-	      $scope.reverseSort = true;
-	   }else{
-	      $scope.users = sortByKeyDescending($scope.users, header);
-	      $scope.reverseSort = false;
-	    }
-	    $scope.paginate($scope.users);
-	};  
-	  
-	function sortByKeyAscending(array, key) {
-	    return array.sort(function(a, b) {
-			var x = a[key]; var y = b[key];
-			return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+		 $scope.submitOnClick = function () {
+			var oneList = [];
+			for(var i in $scope.pagedList){
+			   for(var x in $scope.pagedList[i]){
+				   if($scope.pagedList[i][x].id){
+					   oneList.push($scope.pagedList[i][x]);
+				   }
+			   }
+			}
+			
+			for(var i in oneList)
+			{
+			   console.log(oneList[i]);
+			}
+		 };
+
+		 /* get mock data from local function */
+	     users = getData();
+		 if(users.length > 0 && $scope.itemsPerPage > 0)
+		  {
+			 paginate(users);
+		  }
+
+		 function getData() {
+			 var data = [{ "id": 1, "first_name": "Charles", "last_name": "Murray", "email": "cmurray0@netlog.com", "country": "Poland" },
+			 { "id": 2, "first_name": "Eric", "last_name": "Watson", "email": "ewatson1@patch.com", "country": "Colombia" },
+			 { "id": 3, "first_name": "Christopher", "last_name": "Wagner", "email": "cwagner2@51.la", "country": "Nigeria" },
+			 { "id": 4, "first_name": "Sarah", "last_name": "Hughes", "email": "shughes3@mozilla.com", "country": "Brazil" },
+			 { "id": 5, "first_name": "Frances", "last_name": "Ray", "email": "fray4@google.ca", "country": "China" },
+			 { "id": 6, "first_name": "Philip", "last_name": "White", "email": "pwhite5@yelp.com", "country": "Brazil" },
+			 { "id": 7, "first_name": "Peter", "last_name": "Hernandez", "email": "phernandez6@nih.gov", "country": "Mexico" },
+			 { "id": 8, "first_name": "Anne", "last_name": "White", "email": "awhite7@eepurl.com", "country": "China" },
+			 { "id": 9, "first_name": "Julia", "last_name": "Fisher", "email": "jfisher8@naver.com", "country": "Brazil" },
+			 { "id": 10, "first_name": "Stephen", "last_name": "Coleman", "email": "scoleman9@php.net", "country": "Philippines" },
+			 { "id": 11, "first_name": "Helen", "last_name": "Riley", "email": "hrileya@bravesites.com", "country": "China" },
+			 { "id": 12, "first_name": "Dorothy", "last_name": "Welch", "email": "dwelchb@home.pl", "country": "Indonesia" },
+			 { "id": 13, "first_name": "Christopher", "last_name": "Coleman", "email": "ccolemanc@miitbeian.gov.cn", "country": "Czech Republic" },
+			 { "id": 14, "first_name": "Jeremy", "last_name": "Foster", "email": "jfosterd@xing.com", "country": "Colombia" },
+			 { "id": 15, "first_name": "Aaron", "last_name": "Hart", "email": "ahart2r@examiner.com", "country": "Argentina" }];
+			 return data;
+		 } 
+		/* get mock data from local function */
+
+		/* use $http to get data */
+		/*
+		$http.get('data.json').success(function(data) {
+
+		   users = data;
+
+			 if(users.length > 0 && $scope.itemsPerPage > 0)
+			 {
+				paginate(users);
+			 }
 		});
-	 }
-	   
-	 function sortByKeyDescending(array, key) {
-		return array.sort(function(a, b) {
-			var x = a[key]; var y = b[key];
-			return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-		});
-	 }
-
-	 $scope.submitOnClick = function () {
-        var oneList = [];
-    	for(var i in $scope.pagedList){
-    	   for(var x in $scope.pagedList[i]){
-    		   if($scope.pagedList[i][x].id){
-    		       oneList.push($scope.pagedList[i][x]);
-    		   }
-    	   }
-    	}
-    	
-    	for(var i in oneList)
-    	{
-    	   console.log(oneList[i]);
-    	}
-	 };
-
-     /* get mock data from local function */
-     $scope.users = getData();
-     if($scope.users.length > 0 && $scope.itemsPerPage > 0)
-      {
-         $scope.paginate($scope.users);
-      }
-
-	 function getData() {
-	     var data = [{ "id": 1, "first_name": "Charles", "last_name": "Murray", "email": "cmurray0@netlog.com", "country": "Poland" },
-         { "id": 2, "first_name": "Eric", "last_name": "Watson", "email": "ewatson1@patch.com", "country": "Colombia" },
-         { "id": 3, "first_name": "Christopher", "last_name": "Wagner", "email": "cwagner2@51.la", "country": "Nigeria" },
-         { "id": 4, "first_name": "Sarah", "last_name": "Hughes", "email": "shughes3@mozilla.com", "country": "Brazil" },
-         { "id": 5, "first_name": "Frances", "last_name": "Ray", "email": "fray4@google.ca", "country": "China" },
-         { "id": 6, "first_name": "Philip", "last_name": "White", "email": "pwhite5@yelp.com", "country": "Brazil" },
-         { "id": 7, "first_name": "Peter", "last_name": "Hernandez", "email": "phernandez6@nih.gov", "country": "Mexico" },
-         { "id": 8, "first_name": "Anne", "last_name": "White", "email": "awhite7@eepurl.com", "country": "China" },
-         { "id": 9, "first_name": "Julia", "last_name": "Fisher", "email": "jfisher8@naver.com", "country": "Brazil" },
-         { "id": 10, "first_name": "Stephen", "last_name": "Coleman", "email": "scoleman9@php.net", "country": "Philippines" },
-         { "id": 11, "first_name": "Helen", "last_name": "Riley", "email": "hrileya@bravesites.com", "country": "China" },
-         { "id": 12, "first_name": "Dorothy", "last_name": "Welch", "email": "dwelchb@home.pl", "country": "Indonesia" },
-         { "id": 13, "first_name": "Christopher", "last_name": "Coleman", "email": "ccolemanc@miitbeian.gov.cn", "country": "Czech Republic" },
-         { "id": 14, "first_name": "Jeremy", "last_name": "Foster", "email": "jfosterd@xing.com", "country": "Colombia" },
-         { "id": 15, "first_name": "Aaron", "last_name": "Hart", "email": "ahart2r@examiner.com", "country": "Argentina" }];
-	     return data;
-	 }
-    /* get mock data from local function */
-
-    /* use $http to get data */
-    //$http.get('data.json').success(function(data) {
-
-    //    $scope.users = data;
-
-    //	 if($scope.users.length > 0 && $scope.itemsPerPage > 0)
-    //	 {
-    //		$scope.paginate($scope.users);
-    //	 }
-    //});
-    /* use $http to get data */
-}]);
+		*/
+		/* use $http to get data */
+	}]);
+})(window.angular);
